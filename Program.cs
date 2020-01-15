3862f670f728d42ab086a4f7cc7a5ae062017696
Jen E <jenevans3ten@gmail.com>
2020-01-15 17:02:27 -0500

    asked user to enter a character

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace PracticeDayThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Write a console application to check whether a letter is a vowel or consonant. 
+            // Hint: You can do this problem using a switch or an if condition.
+
+            Console.Write("Enter a letter: ");
+
+            string userInput = Console.ReadLine();
+            char myLetter = Convert.ToChar(userInput);
+            // char myLetter = Convert.ToChar(Console.ReadLine());
+
         }
     }
 }
